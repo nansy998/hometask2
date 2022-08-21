16e818395de5ab7cee5a87dfeca16dd19cc51ed0
Nancy <nastyshazoloto@mail.ru>
2022-08-21 22:22:30 +0300

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите трехзначное число");
+    while(true)
+    {
+        string input = Console.ReadLine();
+        if(!input.Equals("exit"))
+            Console.WriteLine("{0}->{1}",input, input[1]);
+        else
+            break;
+    }
